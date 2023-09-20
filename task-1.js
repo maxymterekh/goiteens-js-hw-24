8817452d66743f6124c46494d91a62edb50a6a1a
maxymterekh <109846960+maxymterekh@users.noreply.github.com>
2023-09-20 20:43:46 +0300

    update

--- task-1.js
+++ task-1.js
@@ -1,5 +1,5 @@
 const button = document.querySelector("#button");
 const textPole = document.querySelector("#input");
 
-button.textContent = input.placeholder;
+button.textContent = textPole.placeholder;
 
